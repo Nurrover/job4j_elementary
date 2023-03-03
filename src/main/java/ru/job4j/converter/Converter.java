fce4affb1597985d0f41525ad05a32030b8d7853
Nurrover <nuriksafarov00@gmail.com>
2023-03-03 13:25:37 +0400

    0. Что такое тестирование [#159431]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,29 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        float in2 = 120;
+        float expected2 = 2;
+        float out2 = Converter.rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("120 rubles are 2. Test result : " + passed2);
+    }
+
+}
