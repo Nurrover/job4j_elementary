e2b8b5e4c315c5748f096b79d69cc9e4f9a3f6c1
Nurrover <nuriksafarov00@gmail.com>
2023-03-18 20:05:04 +0400

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен:" + ages.length);
         System.out.println("Размер массива surnames равен:" + surnames.length);
         System.out.println("Размер массива prices равен:" + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Peter Arsentev";
+        names[1] = "Nurlan Safarov";
+        names[2] = "Korobeinikov Stas";
+        names[3] = "Lana Sergeeva";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
