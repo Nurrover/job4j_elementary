0f4ab77d68159e01925dc9cb409d99a6db9fb4cc
Nurrover <nuriksafarov00@gmail.com>
2023-03-20 21:14:21 +0400

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,13 +2,24 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rsl = -1;
+        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
-                rsl = index;
+                rst  = index;
                 break;
             }
         }
-        return rsl;
+        return rst;
+    }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
     }
 }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -22,4 +22,26 @@ public class FindLoopTest {
         int rsl = FindLoop.indexOf(data, el);
         assertThat(rsl).isEqualTo(expected);
     }
+
+    @Test
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int el = 4;
+        int start = 1;
+        int finish = 8;
+        int expected = 3;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasSomeEqualElementThenMinus1() {
+        int[] data = {-2, 10, 3, 5, 11, 54, 23, 100, -12, 4};
+        int el = 8;
+        int start = 2;
+        int finish = 7;
+        int expected = -1;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
 }
