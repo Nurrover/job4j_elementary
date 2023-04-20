cf3f77f360c029eb262148d420d52afdfbd5c11e
Nurrover <nuriksafarov00@gmail.com>
2023-04-20 16:16:23 +0400

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,9 +1,12 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void plus(int first, int second) {
-        int result = first + second;
-        System.out.println(result);
+    public static double add(double first, double second) {
+        return first + second;
+    }
+
+    public static double add(double first, double second, double third) {
+        return add(first, add(second, third));
     }
 
     public static void minus(int first, int second) {
@@ -12,8 +15,8 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        plus(1, 2);
-        plus(10, 11);
+        System.out.println(add(1, 2));
+        System.out.println(add(10, 11, 24));
         minus(7, 3);
         minus(2, 6);
     }
--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,4 +6,12 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int middle, int right) {
+        return max(middle, max(left, right));
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return max(four, max(first, second, third));
+    }
+
 }
